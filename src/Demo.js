c9fb5f622adeef7957d181b0f92ba47546394865
Sumedha <sparklesoul067@gmail.com>
2025-12-29 18:13:22 +0000

    task in react

--- src/Demo.js
+++ src/Demo.js
@@ -3,54 +3,61 @@ import TestImg from "./happy-woman-standing-indoors-drinking-citrus-beverage_171
 
 function Demo() {
   return (
-    <div> <div>
+    <div>
     <div className="firstPage"> 
         <div className="u-layout">
           
    <img src={TestImg}></img> 
    </div>
-   <div className="center">
+   <div className="intro"> 
    <h1> Prices </h1> 
-   <p> Images from Freepik </p>
-   <button type="button" className="btn"> Work with us </button> </div>
+   <p> Images from Freepik </p> 
+   <button type="button" className="btnn"> Work with us </button> 
+    </div> 
     </div>
-</div>
+   <br></br>
 
 <div>
-    <div>
+    <div className="section">
         <h1> Benefits </h1>
-        <div className="page">
+        <div className="Page">
         <p> Ut enim ad minim veniam, quis nostrud exercitation ullamco 
             laboris nisi ut aliquip ex eacommodo consequat. Duis aute 
             irure dolor in reprehenderit in voluptate velit esse cillum 
-            dolore eu fugiatnulla pariatur.</p></div>
-   <div className="box">
+            dolore eu fugiatnulla pariatur.</p>
+            </div> 
+        <div className="flex-container">
+   <div className="flex-box1">
      <h1> Test Day </h1>
     <h1> $12 </h1>
     <button type="button" className="btn"> Choose Package </button>
+</div>
 
-   <div className="boxx">
+   <div className="flex-box">
     <h1> Basic Bundle </h1>
     <h1> $55 </h1>
     <button type="button" className="btn"> Choose Package </button>
    </div>
-   <div className="boxx">
+
+   <div className="flex-box">
     <h1> Advance Bundle </h1>
     <h1> $150 </h1>
-    <button type="button" className="btn"> Choose Package </button>
-   </div>
+    <button type="button" className="btn"> Choose Package </button> 
     </div>
-</div>
-</div>
-
+    </div>
+</div> 
+</div> 
+<br></br>
 <div>
-    <div className>
-    <h1> Frequently Question Asked </h1>
-    <p> What is Naturopath? </p><br></br>
+    <div className="page1">
+    <h1> Frequently Asked Questions </h1> 
+   {/* <p> What is Naturopath? </p>  */}
+    <disable>  <input type="Name" placeholder="What is Naturopath? ^"></input></disable>
     <p> Answer. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id suscipit ex. Suspendisse rhoncus
         aoreet purus quis elementum. Phasellus sed efficitur dolor, et ultricies sapien. Quisque fringilla sit amet dolor 
         commodo efficitur. Aliquam et sem odio. In ullamcorper nisi nunc, et molestie ipsum iaculis sit amet.</p>
     </div>
+    
     <div className="form-page">
         <form className="form"> 
             <h1> How can i help you? </h1>
@@ -58,18 +65,32 @@ function Demo() {
                 start working together:</p> 
                 <div>
                     {/* <label for="NameFormControlInput1" className="form-label"><ins> Name </ins></label> */}
-                    <input type="Name" placeholder="Enter Name"></input>
+                    <input type="Name" placeholder="Enter Name"></input> 
                 </div>
 
             <div>
                 {/* <label for="exampleFormControlInput1" className="form-label"><ins> Email adresss </ins></label> */}
                 <input type="email" placeholder="Enter Email"></input>
                 </div>
+                
+                <div><br></br>
+                 <label for="exampleFormControlTextarea1" class="form-label">...</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                <div><br></br>
+                     <button type="button" className="btn"> Send Message </button>
+    </div>
         </form>
     </div> 
+    <div>
+        <div>
+            
+        </div>
+    </div>
 </div> 
 </div>
-  )
+
+  ) 
 }
 
 export default Demo
